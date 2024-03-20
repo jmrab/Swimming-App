dc07de66d248b68d306d3d7a892131d85227b520
Jared Rabadam <jmrabadam@gmail.com>
2024-03-20 09:46:19 -0400

    Initial Commit

---
+++ Swimming App/Swimming_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swimming_AppApp.swift
+//  Swimming App
+//
+//  Created by Jared Rabadam on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Swimming_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
