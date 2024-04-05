9b71f3fe88a323a1537c3b36746ec4d03bac64ff
jmrab <111461043+jmrab@users.noreply.github.com>
2024-04-05 09:21:23 -0400

    Added splash screen

--- Swimming App/ContentView.swift
+++ Swimming App/ContentView.swift
@@ -7,26 +7,63 @@
 
 import SwiftUI
 
-struct GreetingScreen: View {
+struct SplashView: View {
+    
+    @State var isActive: Bool = false
+    
     var body: some View {
-        ZStack {
-            Image("1234")
-                .scaleEffect(CGSize(width: 0.8, height: 0.9))
-            Rectangle()
-                .fill(Color.gray)
-                .frame(width: 330, height: 90)
-                .opacity(0.7)
-
-            Text("Swim Tracker")
-                .font(.system(size: 48))
-                .foregroundColor(.white)
-            
-            
+        
+        VStack {
+            if (self.isActive) {
+                MainMenu()
+            } else {
+                ZStack {
+                    Image("loadingImg")
+                        .scaleEffect(CGSize(width: 0.8, height: 0.9))
+                    
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(height: 120)
+                        .edgesIgnoringSafeArea(.horizontal)
+                        .offset(y: -170)
+                        .opacity(0.5)
+                    
+                    Text("Swim Tracker")
+                        .font(.system(size: 60))
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .offset(y: -170)
+                        .shadow(color: .white, radius: 1, x: 1, y: 2)
+                    
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                        withAnimation {
+                            self.isActive = true
+                        }
+                        
+                    }
+                }
+            }
         }
-        .padding()
+        
+    }
+    
+}
+        struct MainMenu: View {
+            var body: some View {
+                Text("bing chilling")
+            }
+        }
+        
+        
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
     }
 }
 
 #Preview {
-    GreetingScreen()
+    SplashView()
 }
